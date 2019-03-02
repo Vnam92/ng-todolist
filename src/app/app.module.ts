6067555bf54ea75e662ecd33350e00c94078743d
vladimir.nam <vnam92@gmail.com>
2019-03-02 20:12:27 +0700

    Reactive forms added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { TodosComponent } from './containers/todos/todos.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TodosComponent, HeaderComponent, TodoListComponent, TodoItemComponent, TodoFormComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
